74fa6946e39646366e1d625412b6c42f8ec8006a
Nishchay <nishchay.gupta@berylsystems.com>
2024-05-21 14:45:34 +0530

    modified to test git flame

--- main.jsx
+++ main.jsx
@@ -1,2 +1,3 @@
-i am trying the git stash command , so i have creasted a new branch to test it
-
+// I am trying the git stash command , so i have creasted a new branch to test it
+<Okay></Okay>
+// Now i am modifiying the content to check the git blame command and see the output.
