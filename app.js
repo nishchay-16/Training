e87c15afcc5b53a098fe7d1505bdd3d3c5536203
Nishchay <nishchay.gupta@berylsystems.com>
2024-05-21 12:31:43 +0530

    Renamed style.css and will try revert comand on this commit

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
-import react ;
-import index.htmn
+import React from 'react';
 
-function use {
-const (a,b)
-} return{
-<div>
-</div>
-)
-export default use;
+const TableComponent = ({ data, columns}) => {
+  return (
+    <div className='table-align'>
+      <table className="table">
+        <thead>
+          <tr>
+            {columns.map((column, index) => (
+              <th key={index}>{column}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((row, rowIndex) => (
+            <tr key={rowIndex}>
+              {columns.map((column, colIndex) => (
+                <td key={colIndex}>{row[column]}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default TableComponent;
