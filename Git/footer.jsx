a90036d1189434e9a20e0e05038a783926f029d4
Nishchay <nishchay.gupta@berylsystems.com>
2024-05-22 10:10:06 +0530

    created footer and removed main.jsx

---
+++ Git/footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './footerstyle.css';
+
+const Footer = () => {
+    return(
+        <div className="footer">
+            <h3>Copyright &copy; Beryl Systems Pvt Ltd 2024</h3>
+        </div>
+    );
+};
+
+export default Footer;
--- Git/main.jsx
+++
@@ -1,3 +0,0 @@
-// I am trying the git stash command , so i have creasted a new branch to test it
-<Okay></Okay>
-// Now i am modifiying the content to check the git blame command and see the output.
