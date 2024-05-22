f4590fcf38802cf5bed9666f52ec4029e9ee60ee
Nishchay <nishchay.gupta@berylsystems.com>
2024-05-22 10:15:35 +0530

    Added file with swapping code

---
+++ Git/main.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './footerstyle.css';
+
+const Footer = () => {
+    return(
+        <div className="footer">
+            <h3>Copyright &copy; Beryl Systems Pvt Ltd 2024</h3>
+        </div>
+    );
+};
+
+export default Footer;
+
+// I am trying the git stash command , so i have creasted a new branch to test it
+// Now i am modifiying the content to check the git blame command and see the output.
--- main.jsx
+++
@@ -1,2 +0,0 @@
-This file is created for trying the cherrypick function in Git.
-I will try to cherrypick this commit from work branch to the main branch.
