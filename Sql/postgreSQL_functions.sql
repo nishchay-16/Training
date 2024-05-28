57ddf1cf440a148fd631460c7435600a515e7d98
Nishchay Gupta <82803501+nishchay-16@users.noreply.github.com>
2024-05-28 13:23:59 +0530

    Learn how to create functions and listed predefined fn

--- Sql/postgreSQL_functions.sql
+++ Sql/postgreSQL_functions.sql
@@ -4,6 +4,26 @@ FUNCTIONS (also referred to as STORED PROCEDURES) are a way to encapsulate SQL c
 They can take arguments, perform operations, and return results.
 PostgreSQL supports functions written in various languages, including SQL, PL/pgSQL (a procedural language for PostgreSQL).
 
+    CREATE A FUNCTION:-
+        syntax:
+                CREATE [OR REPLACE] FUNCTION function_name (arguments) 
+                RETURNS return_datatype AS $variable_name$
+                    DECLARE
+                        declaration;
+                        [...]
+                    BEGIN
+                        < function_body >
+                        [...]
+                        RETURN { variable_name | value }
+                    END; LANGUAGE plpgsql;
+
+        where,
+        function_name: It is the name of the function.
+        arguments: It is a list of input parameters that the function takes.
+        return_datatype: It is the data type that the function returns.
+        function_body: It is the code that the function executes.
+        plpgsql: It is a procedural language for PostgreSQL.
+
 
 DATE & TIME FUNCTIONS :-
 
@@ -205,3 +225,109 @@ DATE & TIME FUNCTIONS :-
         command: SELECT '2020-01-01'::date - INTERVAL '5 days';
         output: 2019-12-27
 
+
+
+
+STRING FUNCTIONS:-
+
+1) LENGTH:-
+        It returns the length of the string.
+        command: SELECT LENGTH('Naman');
+        output: 5
+
+2) UPPER:-
+        It converts the string to uppercase.
+        command: SELECT UPPER('Nishchay');
+        output: NISHCHAY
+
+3) LOWER:-
+        It converts the string to lowercase.
+        command: SELECT LOWER('Nishchay');
+        output: nishchay
+
+4) SUBSTR:-
+        It returns the substring of the string.
+        command: SELECT SUBSTR('Nishchay', 2, 3);
+        output: ish
+
+5) CONCAT:-
+        It concatenates the string.
+        command: SELECT CONCAT('Nishchay', 'Gupta');
+        output: NishchayGupta
+
+
+
+
+MATHEMATICAL FUNCTIONS:-
+
+1) ABS:-
+        It returns the absolute value of the number.
+        command: SELECT ABS(-10);
+        output: 10
+
+2) SQRT:-
+        It returns the square root of the number.
+        command: SELECT SQRT(25);
+        output: 5
+
+3) CEIL:-
+        It returns the smallest integer that is greater than or equal to the number.
+        command: SELECT CEIL(10.5);
+        output: 11
+
+4) FLOOR:-
+        It returns the largest integer that is less than or equal to the number.
+        command: SELECT FLOOR(10.5);
+        output: 10
+
+5) Round:-
+        It rounds the number to the specified number of decimal places.
+        command: SELECT ROUND(10.5);
+        output: 11
+
+6) TRUNC:-
+        It truncates the number to the specified number of decimal places.
+        command: SELECT TRUNC(10.5);
+        output: 10  
+
+7) MOD:-
+        It returns the remainder of the division of the two numbers.
+        command: SELECT MOD(10, 3);
+        output: 1
+
+8) POWER:-  
+        It returns the result of raising the first number to the power of the second number.
+        command: SELECT POWER(2, 3);
+        output: 8
+
+9) RANDOM:-
+        It returns a random number between 0 and 1.
+        command: SELECT RANDOM();
+        output: 0.4610965802828153
+
+
+
+AGGREGATE FUNCTIONS:-
+
+1) AVG:-
+        It returns the average of the numbers.
+        command: SELECT avg(salary) FROM employees;  
+
+2) COUNT:-
+        It returns the number of rows in the table.
+        command: SELECT COUNT(*) FROM employees;
+
+3) MAX:-
+        It returns the maximum value in the column.
+        command: SELECT MAX(salary) FROM employees;
+
+4) MIN:-
+        It returns the minimum value in the column.
+        command: SELECT MIN(salary) FROM employees; 
+
+5) SUM:-
+        It returns the sum of the values in the column.
+        command: SELECT SUM(salary) FROM employees; 
+
+
+        
