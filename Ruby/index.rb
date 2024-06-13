e5c589402d614a7f75252d913af7605e4b378999
Nishchay Gupta <82803501+nishchay-16@users.noreply.github.com>
2024-06-13 16:27:27 +0530

    Learned what is Ruby & some basic features

---
+++ Ruby/index.rb
@@ -0,0 +1,20 @@
+puts "Hello, Nishchay Gupta." 
+
+END {
+   puts "Completed Ruby Learning!"
+}
+
+BEGIN {
+   puts "This is Ruby Learning!"
+}
+
+# I am a comment. Just ignore me.
+
+=begin
+This is a comment.
+This is a comment, too.
+This is a comment, too.
+I said that already.
+=end
+
+
