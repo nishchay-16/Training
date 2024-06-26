a24bfdb1b7d1ff1f56b8fa67c96549db4ac4b339
Nishchay Gupta <82803501+nishchay-16@users.noreply.github.com>
2024-06-26 14:48:25 +0530

    Tried the string methods again to learn

--- Ruby/index.rb
+++ Ruby/index.rb
@@ -1,12 +1,12 @@
-puts "Hello, Nishchay Gupta." 
+# puts "Hello, Nishchay Gupta." 
 
-END {
-   puts "Completed Ruby Learning!"
-}
+# END {
+#    puts "Completed Ruby Learning!"
+# }
 
-BEGIN {
-   puts "This is Ruby Learning!"
-}
+# BEGIN {
+#    puts "This is Ruby Learning!"
+# }
 
 # I am a comment. Just ignore me.
 
@@ -17,4 +17,40 @@ This is a comment, too.
 I said that already.
 =end
 
+# Tring Sring Methods :-
 
+str = "Hello, World!"
+puts str.length
+puts str.empty?
+puts str.count("l") 
+str.insert(1, "ey")
+puts str
+puts str.upcase
+puts str.downcase 
+puts str.swapcase  
+puts str.capitalize
+puts str.reverse 
+puts str.split(" ")
+puts str.chop
+puts str.strip  
+str1="1234nishchay"
+puts str1.to_i  
+puts str1.to_r 
+puts str.gsub("World", "Ruby") 
+str << " Ruby"
+puts str 
+puts str.index("o")    
+puts str.rindex("o")   
+puts str.succ 
+puts str.start_with?("Hello")  
+puts str.end_with?("Ruby")  
+# str.clear
+# puts str  
+puts str.to_f  
+puts str.to_c
+puts str.chars
+# str = "Hello, World!"
+puts str.slice(7, 5) 
+str = "abc123"
+
+puts str.scan(/\d+/)
