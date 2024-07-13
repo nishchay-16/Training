f45c8011a7ac94ff299a368be97c56200aef753a
Nishchay <nishugupta123gupta@gmail.com>
2024-07-13 08:45:02 +0530

    Completed Assignment.

--- Ruby/Assignment/assignment_solution.rb
+++ Ruby/Assignment/assignment_solution.rb
@@ -495,3 +495,448 @@ Ans->
     end 
     
     puts good_pairs([1, 2, 3, 1, 1, 3])
+
+
+
+<---------------------------------------------HASHES-------------------------------------------->
+
+Q29)  Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+        Symbol Value
+        I 1
+        V 5
+        X 10
+        L 50
+        C 100
+        D 500
+        M 1000
+    Write a function which takes roman number and return integer corresponding to that roman number.
+Ans->
+    def roman_to_integer(s)
+        roman = {'I' => 1, 'V' => 5, 'X' => 10, 'L' => 50, 'C' => 100, 'D' => 500, 'M' => 1000}
+        result = 0
+        for i in s.chars
+            result += roman[i]
+        end
+        return result
+    end
+
+    puts roman_to_integer("MCMXCIV")
+    # Output: 2216
+        
+
+Q30) A pangram is a sentence where every letter of the English alphabet appears at least once.
+     Given a string sentence containing only lowercase English letters, return true if sentence is a pangram, or false otherwise.
+Ans-> 
+    def is_pangram(s)
+        letters = s.downcase.scan(/[a-z]/)
+        letters.uniq.length == 26
+    end
+
+    puts is_pangram("The quick brown fox jumps over the lazy dog")
+
+    #Output:
+    true
+
+
+Q31) Given an array of integers nums and an integer target, return indices of the two numbers such that they
+add up to target. You may assume that each input would have exactly one solution, and you may not use
+the same element twice. You can return the answer in any order.
+Ans-> 
+def two_sum(nums, target)
+    hash = {}
+    a=0
+    for i in 0...nums.length
+      a=target-nums[i]
+      if hash.include?(a)
+        return nums.index(a),i
+      else
+        hash.store(nums[i],nil)
+      end
+    end
+  end
+  nums=[3,2,4]
+  print two_sum(nums,6)
+
+#Output:
+[1, 2] 
+
+
+Q32) Given a string s, return true if s is a good string, or false otherwise. A string s is good if all the characters
+that appear in s have the same number of occurrences (i.e., the same frequency).
+Ans->
+def are_occurrences_equal(s)
+    arr = s.chars.uniq
+    count=[]
+  for i in 0..arr.length-1
+    count.append(s.count(arr[i]))
+  end
+    if count.uniq.size==1
+        return true
+    else
+        return false
+    end
+end
+
+puts are_occurrences_equal("aabbccdd")
+#Output:
+true
+
+
+Q33) Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range
+that is missing from the array.
+Ans->
+def missing_number(nums)
+    for i in 0..nums.length
+        if nums.include?(i)
+            next
+        else
+            return i
+        end
+    end
+end
+
+Q34) Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the
+result must be unique and you may return the result in any order.
+Ans->
+def intersection(nums1, nums2)
+    return nums1.intersection(nums2)
+end
+
+puts intersection([1,2,3],[1,4,5])
+#Output:
+[1]
+
+
+Q35) You are given two strings of the same length s and t. In one step you can choose any character of t and
+replace it with another character. Return the minimum number of steps to make t an anagram of s. An
+Anagram of a string is a string that contains the same characters with a different (or the same) ordering.
+Ans->
+def replacing(s, t)
+    def anagram(str1, str2)
+      arr1=str1.chars.sort
+      arr2=str2.chars.sort
+      puts arr1.eql?(arr2)
+    end
+    steps=0
+    for i in 0..s.length-1
+    if anagram(s,t)
+      return steps
+    elsif t.include?(s[i])
+      next
+    else
+      steps+=1
+    end
+    end
+    return steps
+  end
+
+
+Q36)  Given a string text, you want to use the characters of text to form as many instances of the word "balloon"
+      as possible. You can use each character in text at most once. Return the maximum number of instances
+      that can be formed.
+     Example: loonballxballpoon
+     You can create only 2 balloon from example text, so return 2
+Ans->
+def max_instance(text)
+    balloon = {
+      'b' => 0,
+      'a' => 0,
+      'l' => 0,
+      'o' => 0,
+      'n' => 0
+    }
+    for i in 0...text.length
+      if balloon.include?(text[i])
+        balloon[text[i]]+=1
+      end
+    end
+    balloon['l']/=2
+    balloon['o']/=2
+    return balloon.values.min
+  end
+  text='loonballxballpoon'
+  puts max_instance(text)
+
+  #Ouput:
+  2
+
+
+Q37) Given a string s which consists of lowercase or uppercase letters, return the length of the longest
+palindrome that can be built with those letters.Letters are case sensitive, for example, "Aa" is not
+considered a palindrome here.
+Ans->
+def longest_palindrome(s)
+    count=0
+    arr = s.chars.uniq
+    hash = {}
+    for i in 0...arr.length
+      hash.store(arr[i],0)
+    end
+    for i in 0...s.length
+      if hash.include?(s[i])
+        hash[s[i]]+=1
+      end
+    end
+    even=hash.select{|key,value| value%2==0}
+    count+=even.length
+    odd=hash.select{|key,value| value%2!=0}
+    count+=odd.values.max
+    return count
+  end
+  s='bcwsusc'
+  puts longest_palindrome(s)
+
+  #Output:
+  3
+
+
+Q38) Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a
+     given target value.
+        If target is not found in the array, return [-1, -1].
+Ans->
+def getting_day(str1)
+    date=Date.parse(str1)
+    puts date.strftime("%A")
+  end
+  require 'date'
+  getting_day('12/07/2016')
+
+  #Output:
+  Tuesday
+
+
+Q39) Given two unsorted arrays A of size N and B of size M of distinct elements, the task is to find all pairs from
+both arrays whose sum is equal to X.
+Ans->
+def pairs(a, b, x)
+    arr=[]
+    for i in 0..a.length-1
+      for j in 0..b.length-1
+        if a[i]+b[j]==x
+          arr.append([a[i],b[j]])
+        end
+      end
+    end
+    return arr
+  end
+  a=[2,3,4,5,6]
+  b=[7,6,5,4,2]
+  x=9
+  print pairs(a,b,x)
+
+  #Output:
+    [[2, 7], [3, 6], [4, 5], [5, 4]]
+
+
+    
+<---------------------------------------Date & Time---------------------------------------------->
+
+Q40)  Write a function which takes year and returns how many Saturday 14ths there are in a given year.
+Ans->
+def saturday(year)
+    count=0
+    for i in 1..12
+    if Date.new(year,i,14).strftime('%A')=='Saturday'
+      count+=1
+    end
+    end
+    return count
+  end
+  require 'date'
+  puts saturday(2021)
+
+#Output:
+1
+
+
+Q41)  Create a function that converts dates from one of five string formats:
+            "January 9, 2019" (MM D, YYYY)
+            "Jan 9, 2019" (MM D, YYYY)
+            "01/09/2019" (MM/DD/YYYY)
+            "01-09-2019" (MM-DD-YYYY)
+            "01.09.2019" (MM.DD.YYYY)
+      The return value will be an array formatted like: [MM, DD, YYYY], where MM, DD, and YYYY are all integers.
+Ans->
+def convert(str)
+    arr=[]
+    if str.include?(",")
+      date=Date.parse(str)
+    end
+    if str.include?("/")
+      date=Date.parse(str, "%m/%d/%Y")
+    end
+    if str.include?('-')
+      date=Date.parse(str, "%m-%d-%Y")
+    end
+    if str.include?('.')
+      date=Date.parse(str, "%m.%d.%Y")
+    end
+    str_date = date.strftime("%m %d %Y")
+    arr = str_date.split(" ") 
+    return arr
+  end
+  require 'date'
+  print convert("01/09/2019")
+
+#Output:
+["09", "01", "2019"] 
+
+
+Q42) Create a function that takes time1 and time2 and return how many hours and minutes have passed
+     between the two times.
+Ans->
+def difference(str1, str2)
+    time1 = Time.parse(str1)
+    time2 = Time.parse(str2)
+    a=(time1-time2)/60
+    b=(time1-time2)/3600
+    return a,b
+  end
+  require 'time'
+  puts difference('22:00:00','02:00:00')
+
+#Output:
+[1200.0, 20.0]
+
+
+Q43) Write a function that, given a date (in the format MM/DD/YYYY), returns the day of the week as a string.
+    Each day name must be one of the following strings: "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", or "Saturday".
+    To illustrate, the day of the week for "12/07/2016" is "Wednesday".
+Ans->
+def getting_day(str1)
+    date=Date.parse(str1)
+    puts date.strftime("%A")
+  end
+  require 'date'
+  getting_day('12/07/2016')
+
+  #Output:
+  Tuesday
+
+
+Q44) Create a function that takes the month and year (as integers) and returns the number of days in that month.
+Ans->
+def total_days(m, y)
+    Date.new(y,m,-1).day
+  end
+  require 'date'
+  puts total_days(2,2020)
+
+#Output:
+29
+
+
+Q45) Given a range of years as a string, return the number of leap years there are within the range (inclusive).
+Ans->
+def leap_year(y1, y2)
+    arr=[]
+    while y1<y2
+      if Date.new(y1).leap?
+        arr.append(y1)
+      end
+      y1+=1
+    end
+    return arr
+  end
+  require 'date'
+  puts leap_year(2000,2010)
+
+#Output:
+[2000, 2004, 2008]
+
+
+46) Given the parameters day, month and year, return whether that date is a valid date.
+is_valid_date(35, 2, 2020) ➞ false
+# February doesn't have 35 days.
+Ans->
+def valid_date(day,month,year)
+    begin 
+      Date.new(year,month,day)
+    rescue
+      puts 'Invalid Date'
+    end
+  end
+  require 'date'
+  puts valid_date(30,8,2022)
+
+# Output:
+2022-08-30
+
+
+47) Given the month and year as numbers, return whether that month contains a Friday 13th.
+Ans->
+def friday(month, year)
+    if Date.new(year,month,13).strftime('%A')=='Friday'
+      return true
+    else
+      return false
+    end
+  end
+  require 'date'
+  puts friday(3,2021)
+
+#Output:
+false
+
+
+48) Given a date, return how many days date is away from 2023 (end date not included). date will be in
+mm/dd/yyyy format.
+Ans->
+def far_away(date)
+    return (Date.parse(date)-Date.new(2022,12,31)).to_i.abs
+end
+require 'date'
+puts far_away('19/08/2021')
+
+#Output:
+499
+
+
+49) The 2nd of February 2020 is a palindromic date in both dd/mm/yyyy and mm/dd/yyyy format
+(02/02/2020). Given a date in dd/mm/yyyy format, return true if the date is palindromic in both date
+formats, otherwise return false
+Ans->
+def palindrom(date)
+    date1=Date.parse(date).strftime('%d%m%Y')
+      date2=Date.parse(date).strftime('%m%d%Y')
+      date1.reverse.eql?(date2)
+    end
+    require 'date'
+    puts palindrom('02/02/2020')
+
+    #Output:
+    true
+
+
+50) If today was Monday, in two days, it would be Wednesday.
+Create a function that takes in an array of days as input and the number of days to increment by. Return an
+array of days after n number of days has passed.
+after_n_days(["Thursday", "Monday"], 4) ➞ ["Monday", "Friday"]
+Ans-> 
+def increment(days, num)
+    day = {
+      0 => 'Sunday',
+      1 => 'Monday', 
+      2 => 'Tuesday',
+      3 => 'Wednesday',
+      4 => 'Thursday', 
+      5 => 'Friday',
+      6 => 'Saturday'
+    }
+    arr = []
+    a=0
+    for i in 0..days.length-1
+      a=day.key(days[i])
+      if a+num>6
+        arr.append(day.fetch((a+num)-7))
+      elsif
+        arr.append(day.fetch(a+num))
+      end
+    end
+    return arr
+  end
+  days = ["Thursday", "Monday"]
+  print increment(days,4)
+
+   # Output:-
+  ["Monday", "Friday"]
