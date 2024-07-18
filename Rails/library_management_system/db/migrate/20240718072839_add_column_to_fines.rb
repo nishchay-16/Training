4f955b370488a5b7632e35beceea226287ab0853
Nishchay <nishugupta123gupta@gmail.com>
2024-07-18 16:24:12 +0530

    Learnt commands to run certain sets of migrations.

--- Rails/library_management_system/db/migrate/20240718072839_add_column_to_fines.rb
+++
@@ -1,5 +0,0 @@
-class AddColumnToFines < ActiveRecord::Migration[7.1]
-  def change
-    add_column :fines, :fine_day, :string
-  end
-end
--- Rails/library_management_system/db/migrate/20240718074526_remove_column_from_fines.rb
+++
@@ -1,5 +0,0 @@
-class RemoveColumnFromFines < ActiveRecord::Migration[7.1]
-  def change
-    remove_column :fines, :fine_day, :string
-  end
-end
