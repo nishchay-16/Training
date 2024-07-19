c450ad6777fbc6c11bb92495b6fa9700622e8ed9
Nishchay <nishugupta123gupta@gmail.com>
2024-07-19 14:51:16 +0530

    Rename the table name using migration

--- Rails/library_management_system/db/schema.rb
+++ Rails/library_management_system/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_19_091329) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_19_091940) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,13 +19,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_19_091329) do
     t.string "nationality"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "authors_books", id: false, force: :cascade do |t|
-    t.bigint "book_id", null: false
-    t.bigint "author_id", null: false
-    t.index ["author_id"], name: "index_authors_books_on_author_id"
-    t.index ["book_id"], name: "index_authors_books_on_book_id"
   end
 
   create_table "books", force: :cascade do |t|
@@ -39,6 +32,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_19_091329) do
     t.bigint "genre_id", null: false
     t.index ["author_id"], name: "index_books_on_author_id"
     t.index ["genre_id"], name: "index_books_on_genre_id"
+  end
+
+  create_table "books_authors_join_table", id: false, force: :cascade do |t|
+    t.bigint "book_id", null: false
+    t.bigint "author_id", null: false
+    t.index ["author_id"], name: "index_books_authors_join_table_on_author_id"
+    t.index ["book_id"], name: "index_books_authors_join_table_on_book_id"
   end
 
   create_table "fines", force: :cascade do |t|
