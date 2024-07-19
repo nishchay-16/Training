d5ffc71ecb6ed1712dd87c97c552f23a6275795f
Nishchay <nishugupta123gupta@gmail.com>
2024-07-19 18:44:10 +0530

    Inserted data in author table using seed

--- Rails/library_management_system/db/seeds.rb
+++ Rails/library_management_system/db/seeds.rb
@@ -7,3 +7,10 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Author.delete_all
+
+Author.create([
+  { author_name: 'Nishchay', nationality: 'Indian' },
+  { author_name: 'Naman', nationality: 'British' }
+])
