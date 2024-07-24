8271b033b410628e263470e3235e876c60c5366a
Nishchay <nishugupta123gupta@gmail.com>
2024-07-24 12:18:31 +0530

    implemented the conditional validations

--- Rails/library_management_system/app/models/transaction.rb
+++ Rails/library_management_system/app/models/transaction.rb
@@ -1,3 +1,30 @@
 class Transaction < ApplicationRecord
-  belongs_to :member
+  # belongs_to :member
+
+   # Conditional validations
+
+  #  validates :isbn, presence: true, if: :book_present?
+  #  def book_present?
+  #    book_id.present?
+  #  end
+
+
+  #  validates :isbn, presence: true, unless: Proc.new { |t| t.issuedate.blank? }   # using proc
+  #  validates :isbn, presence: true, unless: -> { issuedate.blank? }               # using lambda
+
+
+  def involves_special_member?
+    member_id == 1
+  end
+  with_options if: :involves_special_member? do |transaction|
+    transaction.validates :isbn, presence: true
+    transaction.validates :book_id, presence: true
+    transaction.validates :issuedate, presence: true
+  end
+
+
+  # validates :isbn, :book_id, :issuedate, presence: true,
+  # if: -> { member_id == 1 },
+  # unless: -> { returndate.present? }
+
 end
