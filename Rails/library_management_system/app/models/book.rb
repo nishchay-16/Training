cbf0b2a87885004ecf73694fa0fb2c9085ae8c3a
Nishchay <nishugupta123gupta@gmail.com>
2024-07-24 13:31:37 +0530

    Worked with validation errors

--- Rails/library_management_system/app/models/book.rb
+++ Rails/library_management_system/app/models/book.rb
@@ -1,2 +1,9 @@
 class Book < ApplicationRecord
+    validates :title, presence: true, length: { minimum: 5 }
+
+    # validate do |book|
+    #     # errors.add :title, :too_plain, message: "not cool enough"
+    #     errors.add :base, :invalid, message: "This person is invalid because ..."
+    #   end
+
 end
