8f0a7dab4e34f81d0c87bfb07a847807671aa378
Nishchay <nishugupta123gupta@gmail.com>
2024-07-25 16:00:35 +0530

    Methods added by belongs_to association reference

--- Rails/library_management_system/app/models/author.rb
+++ Rails/library_management_system/app/models/author.rb
@@ -1,11 +1,13 @@
 class Author < ApplicationRecord
   has_many :books, dependent: :destroy
+
     # Custom validation method
-    validate :check_author_name_format
-    def check_author_name_format
-      unless author_name.match?(/\A[A-Za-z]+\z/)
-        errors.add(:author_name, "must only contain letters")
-      end
-    end
+    
+    # validate :check_author_name_format
+    # def check_author_name_format
+    #   unless author_name.match?(/\A[A-Za-z]+\z/)
+    #     errors.add(:author_name, "must only contain letters")
+    #   end
+    # end
     
   end
