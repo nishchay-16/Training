16f0ce115a3475015c852be84b89e724a1b61e22
Nishchay <nishugupta123gupta@gmail.com>
2024-07-27 13:42:53 +0530

    Added before_save , after_save and around_save callbacks

--- Rails/library_management_system/app/models/article.rb
+++ Rails/library_management_system/app/models/article.rb
@@ -1,9 +1,12 @@
 class Article < ApplicationRecord
-  validates :title, presence: true
+  # validates :title, presence: true
   validates :content, length: { minimum: 10 }
 
   before_validation :normalize_title                   # before_validation callback
   after_validation :set_default_content                # after_validation callback
+  before_save :ensure_title_has_a_value                # before_save callback
+  around_save :log_save_actions                        # around_save callback
+  after_save :update_search_index                      # after_save callback
 
   private
 
@@ -14,6 +17,20 @@ class Article < ApplicationRecord
 
   def set_default_content
     self.content = "Default content" if content.blank?
+  end
+
+  def ensure_title_has_a_value
+    self.title = "Untitled" if title.blank?
+  end
+
+  def log_save_actions
+    puts "Around Save: Before Save"
+    yield
+    puts "Around Save: After Save"
+  end
+
+  def update_search_index
+    puts "After Save: Updated search index"
   end
 
 end
--- Rails/rails_callbacks.rb
+++ Rails/rails_callbacks.rb
@@ -27,3 +27,39 @@ With callbacks it is possible to write code that will run whenever an Active Rec
       3.3.0 :160 > article.content
       => "Default content" 
 
+3) before_save -> Registers a callback to be called before a record is saved.
+      Example:
+      3.3.0 :176 > article = Article.new(content: "This is some content.")
+      => #<Article:0x0000000120e92a48 id: nil, title: nil, content: "This is some content.", created_at: nil, updated_at: nil> 
+      3.3.0 :177 > article.save
+       TRANSACTION (0.3ms)  BEGIN
+       Article Create (6.2ms)  INSERT INTO "articles" ("title", "content", "created_at", "updated_at") VALUES ($1, $2, $3, $4) RETURNING "id"  [["title", "Untitled"], ["content", "This is some content."], ["created_at", "2024-07-27 07:59:23.935535"], ["updated_at", "2024-07-27 07:59:23.935535"]]
+       TRANSACTION (0.7ms)  COMMIT
+      => true 
+      3.3.0 :178 > article.title
+      => "Untitled" 
+
+4) around_save -> Registers a callback to be called around the save of a record. 
+      Example:
+      3.3.0 :180 > article = Article.new(title: "test title", content: "This is some content.")
+      => #<Article:0x000000011fadf7d0 id: nil, title: "test title", content: "This is some content.", created_at: nil, updated_at: nil> 
+      3.3.0 :181 > article.save
+      Around Save: Before Save
+       TRANSACTION (0.4ms)  BEGIN
+       Article Create (2.8ms)  INSERT INTO "articles" ("title", "content", "created_at", "updated_at") VALUES ($1, $2, $3, $4) RETURNING "id"  [["title", "Test Title"], ["content", "This is some content."], ["created_at", "2024-07-27 08:07:04.150654"], ["updated_at", "2024-07-27 08:07:04.150654"]]
+      Around Save: After Save
+       TRANSACTION (1.3ms)  COMMIT
+      => true 
+
+5) after_save -> Registers a callback to be called after a record is saved.
+      Example:
+      => #<Article:0x00000001216d5c00 id: nil, title: "test title", content: "This is some content.", created_at: nil, updated_at: nil> 
+      3.3.0 :185 > article.save
+      3.3.0 :186 > 
+      Around Save: Before Save
+        TRANSACTION (0.2ms)  BEGIN
+        Article Create (2.1ms)  INSERT INTO "articles" ("title", "content", "created_at", "updated_at") VALUES ($1, $2, $3, $4) RETURNING "id"  [["title", "Test Title"], ["content", "This is some content."], ["created_at", "2024-07-27 08:10:04.125095"], ["updated_at", "2024-07-27 08:10:04.125095"]]
+      Around Save: After Save
+      After Save: Updated search index
+        TRANSACTION (1.4ms)  COMMIT
+       => true     
