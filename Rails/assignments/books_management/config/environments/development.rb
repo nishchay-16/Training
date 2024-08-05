3abfa36782d722ebb3f86f0baa33baa610ea96f8
Nishchay <nishugupta123gupta@gmail.com>
2024-08-05 15:43:48 +0530

    set up the default URL options for the Devise mailer.

--- Rails/assignments/books_management/config/environments/development.rb
+++ Rails/assignments/books_management/config/environments/development.rb
@@ -73,4 +73,20 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+
+  config.action_controller.raise_on_missing_callback_actions = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:         'smtp.gmail.com',
+    port:            587,
+    domain:          'google.com',
+    user_name:       ENV['SMTP_USER_NAME'],
+    password:        ENV['SMTP_PASSWORD'],
+    authentication:  'plain',
+    enable_starttls: true,
+    open_timeout:    5,
+    read_timeout:    5 }
+
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
 end
