9a4061cc851680cdae2b8aef48f018ed138c3e64
Nishchay <nishugupta123gupta@gmail.com>
2024-09-11 21:47:36 +0530

    Successfully scheduled the msg in slack using sidekiq

---
+++ Rails/assignments/library_management_system/app/sidekiq/slack_message_job.rb
@@ -0,0 +1,31 @@
+require 'net/http'
+require 'json'
+
+class SlackMessageJob
+  include Sidekiq::Job
+
+  sidekiq_options queue: 'default'
+
+  def perform(message, channel = '#general')
+    uri = URI(ENV['SLACK_WEBHOOK_URL'])
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = (uri.scheme == 'https')
+
+    request = Net::HTTP::Post.new(uri.path, {'Content-Type' => 'application/json'})
+    request.body = { text: message, channel: channel }.to_json
+
+    begin
+      response = http.request(request)
+
+      if response.is_a?(Net::HTTPSuccess)
+        Rails.logger.info "Message sent successfully: #{response.body}"
+      else
+        Rails.logger.error "Failed to send message: #{response.body}"
+      end
+    rescue Errno::ECONNRESET => e
+      Rails.logger.error "Connection reset error: #{e.message}"
+    rescue => e
+      Rails.logger.error "An error occurred: #{e.message}"
+    end
+  end
+end
