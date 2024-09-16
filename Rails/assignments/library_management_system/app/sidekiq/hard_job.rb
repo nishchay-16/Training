0d9ccdde40e4c7226e509a00c5c3e1314a170c1e
Nishchay <nishugupta123gupta@gmail.com>
2024-09-16 19:03:25 +0530

    Added an array of tag to the job

--- Rails/assignments/library_management_system/app/sidekiq/hard_job.rb
+++ Rails/assignments/library_management_system/app/sidekiq/hard_job.rb
@@ -1,6 +1,6 @@
 class HardJob
   include Sidekiq::Job
-  sidekiq_options queue: 'critical'
+  sidekiq_options queue: 'critical', retry: 5 ,tags: ['alpha', '🥇']
 
   def perform(name, count)
     puts "Performing hard job for #{name}, #{count} times"
