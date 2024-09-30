b1a143a56ae08c47b8490570344360496c7a4dbb
Nishchay <nishugupta123gupta@gmail.com>
2024-09-30 11:16:54 +0530

    Generated model & created table "posts"

--- Rails/assignments/library_management_system/db/schema.rb
+++ Rails/assignments/library_management_system/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_05_091945) do
+ActiveRecord::Schema[7.1].define(version: 2024_09_30_053817) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,6 +23,14 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_05_091945) do
     t.bigint "accountable_id"
     t.index ["accountable_type", "accountable_id"], name: "index_accounts_on_accountable"
     t.index ["supplier_id"], name: "index_accounts_on_supplier_id"
+  end
+
+  create_table "admins", force: :cascade do |t|
+    t.string "emails", default: [], array: true
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.datetime "scheduled_time"
+    t.string "csv_file"
   end
 
   create_table "articles", force: :cascade do |t|
@@ -172,6 +180,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_05_091945) do
 
   create_table "picture_files", force: :cascade do |t|
     t.string "filepath"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
